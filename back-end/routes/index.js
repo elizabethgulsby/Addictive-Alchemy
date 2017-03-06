430e8b58b8cca7b6245bf9c74ef91fa9b5711991
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-03-06 15:55:19 -0500

    created database

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var config = require('..config/config');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	user: config.user,
+	password: config.password,
+	host: config.host,
+	database: config.database
+});
+
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	var sideEffectsQuery = "SELECT side_effect_name FROM side_effects WHERE set_id = 5";
+  	connection.query(sideEffectsQuery, (error, results, fields) => {
+  		if (error) throw error;
+  		res.json({
+  			msg: "Sanity Check for Back End."
+  		});
+  	});
 });
 
 module.exports = router;
