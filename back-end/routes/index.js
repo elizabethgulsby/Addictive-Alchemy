33688ace54da4ff8dc93e7a71bb92e265eaabff7
Elizabeth Gulsby <elizabeth.gulsby@gmail.com>
2017-03-09 16:11:51 -0500

    Completed Register logic; began styling random side effects; began constructing random side effect generation query

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -10,10 +10,19 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+//password encryption
+var bcrypt = require('bcrypt-nodejs');
+var hashedPassword = bcrypt.hashSync('x');
+console.log(hashedPassword)
+var checkHash = bcrypt.compareSync('x', hashedPassword);
+console.log(checkHash)
+
+
 
 /* GET home page. */
+//below is a test query for now
 router.get('/', function(req, res, next) {
-  	var sideEffectsQuery = "SELECT side_effect_name FROM side_effects WHERE set_id = 5";
+  	var sideEffectsQuery = "SELECT DISTINCT *, first_color, second_color FROM side_effects ORDER BY rand() limit 6";
   	connection.query(sideEffectsQuery, (error, results, fields) => {
   		if (error) throw error;
   		res.json(results);
@@ -34,11 +43,61 @@ router.post('/login', function(req, res, next) {
 			});
 		}
 		else {
-			res.json({
-				msg: "Cool!"
-			})
+			// username is valid.  Check password against bcrypt.  Remove any test data added before bcrypt.
+			checkHash = bcrypt.compareSync(password, results[0].password);
+			console.log(checkHash);
+			if (checkHash === false) {
+				//password not found!
+				res.json({
+					msg: "Bad password!"
+				})
+			}
+			else {
+				//password found, username checks out - add token that keeps user logged in for 1 hr max
+				var token = randtoken.uid(40);
+				insertToken = "UPDATE users SET token = ?, token_exp=DATE_ADD(NOW(), INTERVAL 1 HOUR) " + "WHERE username = ?";
+				connection.query(insertToken, [token, username], (error, results, fields) => {
+					console.log(token);
+					res.json({
+						msg: "User exists! Insert token.",
+						token: token,
+						username: req.body.username
+					});
+				});
+			}
 		}
 	});
 });
 
+//get the registration page
+router.post('/register', function(req, res, next) {
+	// console.log("test")
+	//first check for dupes
+	checkDupes = "SELECT * FROM users WHERE user_name = ? OR email = ?"
+	console.log("hello");
+	connection.query(checkDupes, [req.body.username, req.body.email], (error, results, fields) => {
+		if (results.length === 0) {
+			// console.log("no dupes")
+			//register this person.  No dupe exists.
+			var insertQuery = "INSERT INTO users (user_name, password, email) VALUES " + "(?, ?, ?)";
+			connection.query(insertQuery, [req.body.username, bcrypt.hashSync(req.body.password), req.body.email], (error2, results2, fields2) => {
+				// console.log(bcrypt.hashSync(req.body.password));
+				if (error2) throw error2;
+				res.json({
+					msg: "User added!"
+				})
+			});
+		}
+		else {
+			// console.log("dupes")
+			res.json({
+				msg: "This user already exists."
+			})
+		}
+	})
+	
+});
+
 module.exports = router;
+
+
